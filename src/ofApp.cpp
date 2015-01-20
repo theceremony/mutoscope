8d246a9b20b4a2d0b5b70f00150ca971a32a9515
Jeramy <theceremony@gmail.com>
2015-01-20 18:49:51 -0500

    some more tweeks

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,6 +6,7 @@ void ofApp::setup(){
     // --------------------------------------
     mvPlayer.loadMovie("test_1.mov");
     mvPlayer.stop();
+	mvPlayer.setSpeed(.28);
 	ofEnableDepthTest();
 	ofEnableLighting();
 	ofEnableAntiAliasing();
